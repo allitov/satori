2ed46abdbe27dc8c7015f05b47542f57983429ed
Alexander Litovkin <litovkinalexan@gmail.com>
2026-02-02 22:25:19 +0700

    chore: add spotless
    - Spotless formatting is applied automatically when you commit.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,59 @@
 plugins {
-    id("java")
+    id("com.diffplug.spotless") version "8.2.1" apply false
 }
 
-repositories {
-    mavenCentral()
+allprojects {
+    repositories {
+        mavenCentral()
+    }
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "com.diffplug.spotless")
+
+    configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(25))
+        }
+    }
+
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        java {
+            googleJavaFormat("1.33.0").aosp()
+            removeUnusedImports()
+            trimTrailingWhitespace()
+            endWithNewline()
+            targetExclude("**/build/**", "**/generated-sources/**", "**/generated/**")
+        }
+    }
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.register<Copy>("copyGitHooks") {
+    description = "Copies git hooks from project directory to .git/hooks"
+    group = "git"
+
+    val gitDir = File("$rootDir/.git")
+
+    onlyIf { gitDir.exists() }
+
+    from("$rootDir/git/hooks")
+    into("${gitDir.path}/hooks")
+
+    filePermissions {
+        user { read = true; write = true; execute = true }
+
+        group { read = true; execute = true }
+
+        other { read = true; execute = true }
+    }
+
+    doLast {
+        println("Git hooks installed successfully into ${gitDir.path}/hooks")
+        file("$rootDir/git/hooks").listFiles()?.forEach {
+            println("   -> Enabled hook: ${it.name}")
+        }
+    }
 }
+
+tasks.maybeCreate("prepareKotlinBuildScriptModel").dependsOn("copyGitHooks")
